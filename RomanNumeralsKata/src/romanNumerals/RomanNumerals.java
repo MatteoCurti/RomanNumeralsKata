9d3573550360b497ed56d1243b7459f197d94965
MatteoCurti <matteo.curti1@studenti.unimi.it>
2018-05-27 17:34:03 +0200

    GREEN Test Six = VI

--- RomanNumeralsKata/src/romanNumerals/RomanNumerals.java
+++ RomanNumeralsKata/src/romanNumerals/RomanNumerals.java
@@ -4,7 +4,10 @@ public class RomanNumerals {
 
 	public static String arabicToRoman(int arabicNumber) {
 		String romanNumber = "";
-		if (arabicNumber == 5) {
+		if (arabicNumber == 6) {
+			romanNumber += "VI";
+		}
+		else if (arabicNumber == 5) {
 			romanNumber += "V";
 		}
 		else if (arabicNumber == 4) {
